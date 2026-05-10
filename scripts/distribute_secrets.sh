b9d129b57fb610788a3727e4d2c4903a4820f25b
valfur03 <valentin.furmanek@gmail.com>
2026-05-10 12:23:31 +0200

    feat: setup doco-cd

--- scripts/distribute_secrets.sh
+++ scripts/distribute_secrets.sh
@@ -1,14 +1,28 @@
 #!/bin/sh
 
-if [ $# -ne 1 ]
+# Copy a flat directory of secret files into the per-service layout expected
+# by compose.yaml's `secrets:` block.
+#
+# Usage: distribute_secrets.sh <source_dir> [<dest_root>]
+#
+# - <source_dir> is interpreted relative to the current working directory.
+# - <dest_root> defaults to the repo root (parent of this script's directory).
+#   In prod with doco-cd, set this to /etc/my-whole-server/ so files land in
+#   the host-managed dir referenced by ${SECRETS_DIR} in compose.yaml.
+#
+# Source-side filenames are flat (e.g. AUTHELIA_JWT_SECRET); destination paths
+# match what compose.yaml's `secrets: file:` references resolve to.
+
+set -eu
+
+if [ $# -lt 1 ] || [ $# -gt 2 ]
 then
-	printf 'arguments number must be 1\n'
+	printf 'usage: %s <source_dir> [<dest_root>]\n' "$0" >&2
 	exit 1
 fi
 
-cd $(dirname "$0")
-
 SOURCE_DIRECTORY=$1
+DEST_ROOT=${2:-$(dirname "$(dirname "$(realpath "$0")")")}
 
 for i in \
 	AUTHELIA_JWT_SECRET,authelia/secrets/JWT_SECRET \
@@ -18,13 +32,22 @@ for i in \
 	BORGMATIC_ENCRYPTION_PASSPHRASE,borgmatic/secrets/ENCRYPTION_PASSPHRASE \
 	LDAP_ADMIN_PASSWORD,ldap/secrets/ADMIN_PASSWORD \
 	NEXTCLOUD_STORAGE_PASSWORD,nextcloud/secrets/STORAGE_PASSWORD \
-	SYNAPSE_STORAGE_PASSWORD,synapse/secrets/STORAGE_PASSWORD
+	SMTP_PASSWORD,secrets/SMTP_PASSWORD \
+	SYNAPSE_STORAGE_PASSWORD,synapse/secrets/STORAGE_PASSWORD \
+	VAULTWARDEN_ADMIN_TOKEN,vaultwarden/secrets/ADMIN_TOKEN \
+	OVH_APPLICATION_KEY,ovh/secrets/APPLICATION_KEY \
+	OVH_APPLICATION_SECRET,ovh/secrets/APPLICATION_SECRET \
+	OVH_CONSUMER_KEY,ovh/secrets/CONSUMER_KEY \
+	DOCO_CD_GIT_ACCESS_TOKEN,doco-cd/secrets/GIT_ACCESS_TOKEN \
+	DOCO_CD_WEBHOOK_SECRET,doco-cd/secrets/WEBHOOK_SECRET \
+	DOCO_CD_SOPS_AGE_KEY,doco-cd/secrets/SOPS_AGE_KEY
 do
 	IFS=","
 	set -- $i
 
-	if [ -f  ../$SOURCE_DIRECTORY/$1 ]
+	if [ -f "$SOURCE_DIRECTORY/$1" ]
 	then
-		cp ../$SOURCE_DIRECTORY/$1 ../$2
+		mkdir -p "$DEST_ROOT/$(dirname "$2")"
+		cp "$SOURCE_DIRECTORY/$1" "$DEST_ROOT/$2"
 	fi
 done
