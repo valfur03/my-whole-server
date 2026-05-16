efa209889ecb4fb039ff8b8db8f66e9ef44ee559
Valentin Furmanek <valentin.furmanek@strapi.io>
2026-05-16 16:19:19 +0200

    fix(auth): change executable calls with debian-based since Authelia upgrade

--- authelia/my-entrypoint.sh
+++ authelia/my-entrypoint.sh
@@ -1,8 +1,13 @@
-#/bin/sh
+#!/bin/sh
+set -eu
 
-# Run `envsubst` against ACL config
-envsubst \
-	< /config/configuration.acl.template.yml \
+# Templated ACL: substitute the two required vars with busybox sed.
+# (The upstream Authelia image dropped envsubst when it switched to a
+# busybox-based base in v4.39.x.)
+sed \
+	-e "s|\${BASE_DOMAIN}|${BASE_DOMAIN}|g" \
+	-e "s|\${HOST_IP}|${HOST_IP}|g" \
+	/config/configuration.acl.template.yml \
 	> /config/configuration.acl.yml
 
 exec "$@"
